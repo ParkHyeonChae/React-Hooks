8aa6d2980f06d04788db48537d34f1b7b3aa42cd
ParkHyeonChae <park19996@naver.com>
2020-07-19 02:32:01 +0900

    useConfirm

---
+++ useConfirm/index.js
@@ -0,0 +1,44 @@
+// useConfirm 확인 alert창
+
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+
+
+const useConfirm = (message = "", onConfirm, onCancel) => {
+
+    if(!onConfirm || typeof onConfirm !== "function") {
+        return;
+    }
+    if(onCancel && typeof onCancel !== "function") {
+        return;
+    }
+
+    const confirmAction = () => {
+        if(confirm(message)) {
+            onConfirm(); // 2. true라고 하면 onConfirm 실행
+        } else {
+            onCancel(); // 3. false면 onCancel 실행
+        }
+    }
+
+    return confirmAction
+};
+
+
+const App = () => {
+
+    const confirmButton = () => console.log("Deleting..."); // 4. true면 onConfirm Callback이 와서 이거 실행
+    const cancelButton = () => console.log("Cancel...")
+    const confirmDelete = useConfirm("Are you sure Delete?", confirmButton, cancelButton); // 1. button을 누른순간 useConfirm의 confirmAction
+
+    return (
+        <div className="App">
+            <button onClick={confirmDelete}>DeleteButton</button>
+        </div>
+    )
+};
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
