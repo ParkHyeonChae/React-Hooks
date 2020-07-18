a89568dc2d298513c703ded6672c24002c12745d
ParkHyeonChae <park19996@naver.com>
2020-07-18 21:35:48 +0900

    useTitle

---
+++ useTitle/index.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+
+
+const useTitle = initialTitle => {
+    const [title, setTitle] = useState(initialTitle);
+    const updateTitle = () => {
+        const htmlTitle = document.querySelector("title");
+        htmlTitle.innerText = title;
+    }
+    useEffect(updateTitle, [title]);
+
+    return setTitle;
+}
+
+const App = () => {
+    const titleUpdater = useTitle("Loading...");
+    // setTimeout(() => titleUpdater("Home"), 5000);
+
+    return (
+        <div className="App">
+            <div>Hi</div>
+        </div>
+    )
+};
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
